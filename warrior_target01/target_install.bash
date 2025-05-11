194e7dc8e9d778bf82fe16c6a58cdacc3084a3ad
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-05-11 18:49:37 -0400

    Create target_install.bash

---
+++ warrior_target01/target_install.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script used to setup basic demo range target system (Rocky 9)
+# To Use: "wget https://raw.githubusercontent.com/itsmmmike/testing/refs/heads/main/warrior_target01/target_install.bash" && sudo bash ./target_install.bash"
+
+# Update and Install Packages
+sudo yum update -y
+sudo yum install nginx -y
+sudo yum install wget -y
+sudo yum install vim -y
+
+# Clear Default Nginx Config (Danger!)
+sudo rm /usr/share/nginx/html/*
+
+# Download Nginx Config Files
+sudo wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ItsMmmike/testing/refs/heads/main/warrior_target01/nginx.conf"
+sudo wget -O /usr/share/nginx/html/index.html "https://raw.githubusercontent.com/ItsMmmike/testing/refs/heads/main/warrior_target01/index.html"
+sudo wget -O /usr/share/nginx/html/You_Tried.png "https://raw.githubusercontent.com/ItsMmmike/testing/refs/heads/main/warrior_target01/You_Tried.png"
+sudo wget -O /usr/share/nginx/html/flag_demo.zip "https://raw.githubusercontent.com/ItsMmmike/testing/refs/heads/main/warror_target01/flag_demo.zip"
+
+# Enable and Start Server
+sudo systemctl enable nginx
+sudo systemctl restart nginx
+
+# Enable Firewall Ports
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --remove-service=ssh --permanent
+sudo firewall-cmd --reload
